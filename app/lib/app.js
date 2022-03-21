05fe78544041f8447e51658b15e9fb413acca893
oscar <oscar.vilarsalvador@everis.nttdata.com>
2022-03-21 16:16:56 +0100

    AngularJS Tutorial #13 - ng-click directive

--- app/lib/app.js
+++ app/lib/app.js
@@ -1,30 +1,41 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('myController', ['$scope',function ($scope) {
 
+    $scope.eliminarUsuario = function (usuario){
+        var usuarioEliminado = $scope.usuarios.indexOf(usuario);
+        $scope.usuarios.splice(usuarioEliminado,1)
+    }
+
     $scope.usuarios = [
         {
             nombre:'Oscar',
             apellido: 'Vilar',
-            premio:27,
-            disponible: true
+            premio: 300,
+            disponible: true,
+            cinturon: 'black'
+
+            
         },
         {
             nombre:'Antonio',
             apellido: 'Fernandez',
-            premio:24,
-            disponible: false
+            premio: 200,
+            disponible: true,
+            cinturon: 'yellow'
         },
         {
             nombre:'Pedro',
             apellido: 'Martinez',
-            premio:30,
-            disponible: false
+            premio :350,
+            disponible: false,
+            cinturon: 'red'
         },
         {
             nombre:'Juan',
             apellido: 'Alberto',
-            premio:33,
-            disponible: true
+            premio: 250,
+            disponible: true,
+            cinturon: 'red'
         },
     ];
 
